a1f82c04de9d440465e5f2ae62c0c2ffacb4e21e
xiaosheng <287915338@qq.com>
2023-01-16 23:15:11 +0800

    :heavy_plus_sign: ant-design-vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,7 @@
 <template>
   <h1>VUE3 PC 标准模版</h1>
   <Hello msg="hello" />
-  <ul>
-    <li>开箱即用</li>
-    <li>typescript</li>
-    <li>Vue3</li>
-    <li>支持 tsx</li>
-    <li>eslint</li>
-    <li>简单易用可扩展</li>
-  </ul>
+  <a-button type="primary">and-vue-button</a-button>
 </template>
 
 <script lang="ts">
@@ -22,14 +15,3 @@ export default defineComponent({
   },
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
