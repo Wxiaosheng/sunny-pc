e801043de1acb1d6bfa8b57408358a3875e6361c
xiaosheng <287915338@qq.com>
2023-01-31 07:56:54 +0800

    调整路由, add login

--- src/routes.ts
+++ src/routes.ts
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Layout from './views/Layout.vue'
 import Home from './views/Home.vue'
 import TemplateDetail from './views/TemplateDetail.vue'
+import Login from './views/Login.vue'
+import Editor from './views/Editor.vue'
 
 const routers = createRouter({
   history: createWebHistory(),
@@ -11,10 +13,19 @@ const routers = createRouter({
       component: Layout,
       children: [
         { path: '', name: 'home',  component: Home, },
-        { path: '/detail/:id', name: 'detail',  component: TemplateDetail, meta: { noFooter: true } },
+        { path: 'detail/:id', name: 'detail',  component: TemplateDetail },
       ]
     },
-    
+    { 
+      path: '/editor', 
+      name: 'editor',  
+      component: Editor 
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    }
   ]
 })
 
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div> login user </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'login',
+  setup() {
+    return {}
+  }
+})
+</script>
